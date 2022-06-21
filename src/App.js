891a62791cfaf1e02665f89a852af84d5081f72b
goodwellsundawa <sundawawelly@gmail.com>
2022-06-21 12:42:55 +0700

    commit challenge

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import './App.css';
-
+import "./App.css";
+import React from "react";
+import Header from "./components/Header";
+import Contact from "./components/Contact";
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from "./data/contacts.json";
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+      <Header />
+      {contacts.map((element) => (
+        <Contact data={element} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,21 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
+import "./Contact.css";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = ({ data }) => {
+  return (
+    <div className="contact-card">
+      <div className="contact-img">
+        <img src={data.photo} alt={`Foto ${data.name}`} />
+      </div>
+      <div className="contact-detail">
+        <h1>{data.name}</h1>
+        <p>{data.phone}</p>
+        <p>{data.email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
